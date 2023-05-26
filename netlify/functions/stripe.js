c51b50c5abc285acbc5ccd4bd8b9ccaf40d443d5
ClaudiaGiv <clau_givan@yahoo.com>
2023-05-26 20:34:22 +0300

    Update netlify function

--- netlify/functions/stripe.js
+++ netlify/functions/stripe.js
@@ -1,26 +1,7 @@
 const stripe = require("stripe")(process.env.VUE_APP_STRIPE_SECRET);
 // const { amount, currency = "gbp" } = JSON.parse(event.body);
 //
-// try {
-//     const paymentIntent = await stripe.paymentIntents.create({
-//         amount: 222,
-//         currency: "usd",
-//         automatic_payment_methods: {enabled: true},
-//     });
-//     return {
-//         statusCode: 200, // http status code
-//         body: JSON.stringify({
-//             paymentIntent
-//         }),
-//     };
-// } catch (e) {
-//     return {
-//         statusCode: 404, // http status code
-//         body: "Error",
-//     };
-// }
-
-exports.handler = async (event, context) => {
+try {
     const paymentIntent = await stripe.paymentIntents.create({
         amount: 222,
         currency: "usd",
@@ -32,22 +13,41 @@ exports.handler = async (event, context) => {
             paymentIntent
         }),
     };
-    // const session = await stripe.checkout.sessions.create({
-    //     payment_method_types: ["card"],
-    //     line_items: [
-    //         {
-    //             price_data: {
-    //                 currency: "usd",
-    //                 product_data: {
-    //                     name: "T-shirt",
-    //                 },
-    //                 unit_amount: 2000,
-    //             },
-    //             quantity: 1,
-    //         },
-    //     ],
-    //     mode: "payment",
-    //     success_url: "https://serverless-payments.netlify.app/success",
-    //     cancel_url: "https://serverless-payments.netlify.app/cancel",
-    // });
-};
+} catch (e) {
+    return {
+        statusCode: 404, // http status code
+        body: "Error",
+    };
+}
+
+// exports.handler = async (event, context) => {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount: 222,
+//         currency: "usd",
+//         automatic_payment_methods: {enabled: true},
+//     });
+//     return {
+//         statusCode: 200, // http status code
+//         body: JSON.stringify({
+//             paymentIntent
+//         }),
+//     };
+//     // const session = await stripe.checkout.sessions.create({
+//     //     payment_method_types: ["card"],
+//     //     line_items: [
+//     //         {
+//     //             price_data: {
+//     //                 currency: "usd",
+//     //                 product_data: {
+//     //                     name: "T-shirt",
+//     //                 },
+//     //                 unit_amount: 2000,
+//     //             },
+//     //             quantity: 1,
+//     //         },
+//     //     ],
+//     //     mode: "payment",
+//     //     success_url: "https://serverless-payments.netlify.app/success",
+//     //     cancel_url: "https://serverless-payments.netlify.app/cancel",
+//     // });
+// };
